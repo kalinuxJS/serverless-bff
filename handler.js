9b97cf1d4d1ade465fc67e2c008b7be78ee7fee9
张文静 <zwj@zhangwenjingdeMacBook-Pro.local>
2025-02-10 14:33:19 +0800

    fix: reverse

--- handler.js
+++ handler.js
@@ -2,7 +2,7 @@ exports.hello = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: "Hello from Serverless BFFx!",
+      message: "Hello from Serverless BFF!",
     }),
   };
 };
